51eb7e7a56ef8adb9d0f55276d819ee62088aadb
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-06-03 16:30:17 +0500

    save & get data from localStorage

--- index.js
+++ index.js
@@ -3,26 +3,39 @@ const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
+let leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+console.log(leadsFromLocalStorage);
+// 1. Check if leadsFromLocalStorage is truthy
+// 2. If so, set myLeads to its value and call renderLeads()
+
+if (leadsFromLocalStorage) {
+  myLeads = leadsFromLocalStorage;
+  renderLeads();
+}
+
+
+
+
 inputBtn.addEventListener("click", function() {
-  myLeads.push(inputEl.value);
-  inputEl.value = "";
-    // 2. Call the renderLeads() function
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
     renderLeads()
 })
 
-// 1. Wrap the code below in a renderLeads() function
 function renderLeads() {
     let listItems = ""
     for (let i = 0; i < myLeads.length; i++) {
-      // listItems += "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>";
-      listItems += `
-          <li>
-            <a target='_blank' href='${myLeads[i]}'>
-                ${myLeads[i]}
-            </a>
-          </li>
+        listItems += `
+            <li>
+                <a target='_blank' href='${myLeads[i]}'>
+                    ${myLeads[i]}
+                </a>
+            </li>
         `
     }
-  ulEl.innerHTML = listItems;
+    ulEl.innerHTML = listItems  
 }
+
+
 
