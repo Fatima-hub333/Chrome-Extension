55ac6ca870b19aacb8f2700ba4a56cf8dfe7b7de
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-06-04 11:41:04 +0500

    Use the chorme API to get the tab

--- index.js
+++ index.js
@@ -12,23 +12,14 @@ if (leadsFromLocalStorage) {
   render(myLeads);
 }
 
-const tabs = [
-  {url: "https://www.linkedin.com/in/per-harald-borgen/"}
-]
-
 tabBtn.addEventListener("click", function () {
- //Grab the URL of the current tab!
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      // since only one tab should be active and in the current window at once
-     // the return variable should only have one entry
-     let activeTab = tabs[0]
-     let activeTabId = activeTab.id // or do whatever you need
-  });
+  chrome.tabs.query({ active: true, currentWindow: true }, function tabs() {
+    myLeads.push(tabs[0].url);
+    localStorage.setItem("myLeads", JSON.stringify("myLeads"));
+    render(myLeads);
 
-
-  myLeads.push(tabs[0].url);
-  localStorage.setItem("myLeads", JSON.stringify("myLeads"));
-  render(myLeads);
+  })
+  
 })
 function render(leads) {
     let listItems = ""
