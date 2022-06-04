1cb6784307ca7c56f2090cd45db6d40000d12d83
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-06-04 10:42:22 +0500

    practice of functions with multiple parameters.

--- index.js
+++ index.js
@@ -1,43 +1,16 @@
-let myLeads = []
-const inputEl = document.getElementById("input-el")
-const inputBtn = document.getElementById("input-btn")
-const ulEl = document.getElementById("ul-el")
-const deleteBtn = document.getElementById("delete-btn");
+// Practice
+// const welcomeEl = document.getElementById("welcome-el");
 
-const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+// function greetUser(name, hello) {
+//   welcomeEl.textContent = hello + " " + name + " 👋";
+// }
 
-if (leadsFromLocalStorage) {
-  myLeads = leadsFromLocalStorage;
-  renderLeads();
+// greetUser("Fatima", "Welcome Back");
+
+const welcomeEl = document.getElementById("welcome-el");
+
+function greetUser(greetings, name, emoji) {
+  welcomeEl.textContent = ` ${greetings}, ${name} ${emoji}`;
 }
 
-deleteBtn.addEventListener("dblclick", function () {
-  localStorage.clear();
-  myLeads = [];
-  renderLeads();
-})
-
-
-inputBtn.addEventListener("click", function () {
-  myLeads.push(inputEl.value)
-  inputEl.value = ""
-  localStorage.setItem("myLeads", JSON.stringify(myLeads))
-  renderLeads()
-});
-
-function renderLeads() {
-    let listItems = ""
-    for (let i = 0; i < myLeads.length; i++) {
-        listItems += `
-            <li>
-                <a target='_blank' href='${myLeads[i]}'>
-                    ${myLeads[i]}
-                </a>
-            </li>
-        `
-    }
-    ulEl.innerHTML = listItems  
-}
-
-
-
+greetUser("Welcome Back", "Fatima Zahir", "🔥");
