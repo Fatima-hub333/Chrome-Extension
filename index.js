108e52ab38e7e1f53b40dfcb11310f03fd7b99d7
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-06-04 11:22:37 +0500

    Create the Tab Button

--- index.js
+++ index.js
@@ -4,12 +4,21 @@ const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 const deleteBtn = document.getElementById("delete-btn")
 const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+const tabBtn = document.getElementById("tab-btn");
+
 
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
   render(myLeads);
 }
 
+const tabs = [
+  {url: "https://www.linkedin.com/in/per-harald-borgen/"}
+]
+
+tabBtn.addEventListener("click", function () {
+  console.log(tabs[0].url);
+})
 function render(leads) {
     let listItems = ""
     for (let i = 0; i < leads.length; i++) {
