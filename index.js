e19e865037a18aa09fe92318e66b016b665305c6
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-06-04 10:47:11 +0500

    Practice Numbers as Function Parameters

--- index.js
+++ index.js
@@ -7,10 +7,19 @@
 
 // greetUser("Fatima", "Welcome Back");
 
-const welcomeEl = document.getElementById("welcome-el");
+// const welcomeEl = document.getElementById("welcome-el");
 
-function greetUser(greetings, name, emoji) {
-  welcomeEl.textContent = ` ${greetings}, ${name} ${emoji}`;
+// function greetUser(greetings, name, emoji) {
+//   welcomeEl.textContent = ` ${greetings}, ${name} ${emoji}`;
+// }
+
+// greetUser("Welcome Back", "Fatima Zahir", "🔥");
+
+// Number as function parameters
+
+// Create a function, add(), that adds two numbers together and returns the sum
+function add(num1, num2) {
+  return num1 + num2;
 }
-
-greetUser("Welcome Back", "Fatima Zahir", "🔥");
+console.log(add(3, 4)) //should log 7
+console.log( add(9, 102)) //should log 111
