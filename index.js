f421f58bb1eedff64f3a79fedf1abd3cb77ca774
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-06-04 10:55:55 +0500

    Practice Arrays as parameters

--- index.js
+++ index.js
@@ -18,8 +18,19 @@
 // Number as function parameters
 
 // Create a function, add(), that adds two numbers together and returns the sum
-function add(num1, num2) {
-  return num1 + num2;
+// function add(num1, num2) {
+//   return num1 + num2;
+// }
+// console.log(add(3, 4)) //should log 7
+// console.log( add(9, 102)) //should log 111
+
+// Arrays as parameters
+// Create a function, getFirst(arr), that returns the first item in the array
+function getFirst(arr) {
+  return arr[0];
 }
-console.log(add(3, 4)) //should log 7
-console.log( add(9, 102)) //should log 111
+
+
+// Call it with an array as an argument to verify that it works
+let firstCard = getFirst([1, 2, 3, 4, 5])
+console.log(firstCard)
