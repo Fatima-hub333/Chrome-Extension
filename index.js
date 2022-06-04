67c89229179542170efc663d779dc6897e208546
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-06-04 11:14:10 +0500

    Refactor Render leads to use a parameter

--- index.js
+++ index.js
@@ -1,36 +1,38 @@
-// Practice
-// const welcomeEl = document.getElementById("welcome-el");
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
 
-// function greetUser(name, hello) {
-//   welcomeEl.textContent = hello + " " + name + " 👋";
-// }
-
-// greetUser("Fatima", "Welcome Back");
-
-// const welcomeEl = document.getElementById("welcome-el");
-
-// function greetUser(greetings, name, emoji) {
-//   welcomeEl.textContent = ` ${greetings}, ${name} ${emoji}`;
-// }
-
-// greetUser("Welcome Back", "Fatima Zahir", "🔥");
-
-// Number as function parameters
-
-// Create a function, add(), that adds two numbers together and returns the sum
-// function add(num1, num2) {
-//   return num1 + num2;
-// }
-// console.log(add(3, 4)) //should log 7
-// console.log( add(9, 102)) //should log 111
-
-// Arrays as parameters
-// Create a function, getFirst(arr), that returns the first item in the array
-function getFirst(arr) {
-  return arr[0];
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+  render(myLeads);
 }
 
+function render(leads) {
+    let listItems = ""
+    for (let i = 0; i < leads.length; i++) {
+        listItems += `
+            <li>
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
+                </a>
+            </li>
+        `
+    }
+    ulEl.innerHTML = listItems
+}
 
-// Call it with an array as an argument to verify that it works
-let firstCard = getFirst([1, 2, 3, 4, 5])
-console.log(firstCard)
+deleteBtn.addEventListener("dblclick", function() {
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+})
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+  render(myLeads);
+})
--- practice.js
+++ practice.js
@@ -67,3 +67,40 @@ if (credits > 0) {
 } else {
   console.log("Sorry, you have no credits 😭");
 }
+
+// Practice
+// const welcomeEl = document.getElementById("welcome-el");
+
+// function greetUser(name, hello) {
+//   welcomeEl.textContent = hello + " " + name + " 👋";
+// }
+
+// greetUser("Fatima", "Welcome Back");
+
+// const welcomeEl = document.getElementById("welcome-el");
+
+// function greetUser(greetings, name, emoji) {
+//   welcomeEl.textContent = ` ${greetings}, ${name} ${emoji}`;
+// }
+
+// greetUser("Welcome Back", "Fatima Zahir", "🔥");
+
+// Number as function parameters
+
+// Create a function, add(), that adds two numbers together and returns the sum
+// function add(num1, num2) {
+//   return num1 + num2;
+// }
+// console.log(add(3, 4)) //should log 7
+// console.log( add(9, 102)) //should log 111
+
+// Arrays as parameters
+// Create a function, getFirst(arr), that returns the first item in the array
+function getFirst(arr) {
+  return arr[0];
+}
+
+
+// Call it with an array as an argument to verify that it works
+let firstCard = getFirst([1, 2, 3, 4, 5])
+console.log(firstCard)
